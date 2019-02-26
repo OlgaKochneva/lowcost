c640a9013337d9a6e7128f48545c55f85018d006
TheDoctorZee <a_khamzin@mail.ru>
2019-02-26 13:31:52 +0300

    newDew

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into users(id, username, password, active, first_name, last_name, document_info, birthday)
-values (1, 'EMAIL@MAIL.COM', 'PASSWORD', true, 'admin', 'admin', '1221', '2000-06-05T00:00:00'),
-       (2, 'log', 'pass', true, 'user', 'user', '1221', '2000-06-05T00:00:00');
-
-insert into user_role (user_id, roles)
-values (1, 'ROLE_ADMIN'),
-       (1, 'ROLE_USER'),
-       (2, 'ROLE_USER');
