d5ed2608673d1bd231975cab1a26e67c7ee4503e
Ilia_Stepanov <Ilia_Stepanov@epam.com>
2019-02-27 09:36:15 +0300

    h2 console

--- src/main/java/com/epam/lowcost/configurtation/WebSecurityConfig.java
+++ src/main/java/com/epam/lowcost/configurtation/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .and()
                             .logout()
                             .permitAll();
+        http.headers().frameOptions().disable();
 
     }
 
