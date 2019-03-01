43f4723202cf7d92ef9d2df7428896480fb158c4
AnastasiaBerlina <47319972+AnastasiaBerlina@users.noreply.github.com>
2019-03-01 16:16:04 +0300

    Update LoginController.java

--- src/main/java/com/epam/lowcost/controller/LoginController.java
+++ src/main/java/com/epam/lowcost/controller/LoginController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.time.LocalDateTime;
-
 import static com.epam.lowcost.util.Endpoints.*;
 
 @Controller
