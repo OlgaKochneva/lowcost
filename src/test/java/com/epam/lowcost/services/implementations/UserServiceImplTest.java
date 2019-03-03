778d95f5be1bf13bf481b86bbf7140d29bd7b335
Ilia_Stepanov <Ilia_Stepanov@epam.com>
2019-03-03 15:08:31 +0300

    initial commit

---
+++ src/test/java/com/epam/lowcost/services/implementations/UserServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.epam.lowcost.services.implementations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceImplTest {
+
+    @Test
+    public void findByUsername() {
+    }
+
+    @Test
+    public void getSessionUser() {
+    }
+
+    @Test
+    public void getAllUsers() {
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void blockUser() {
+    }
+
+    @Test
+    public void unblockUser() {
+    }
+}
